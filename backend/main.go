cdcad62d38a7bc40cad23800c928329e6087c0bd
Ryan McCarthy <ryanmccarthy031@gmail.com>
2022-06-13 02:15:43 +0000

    Some go updates

--- backend/main.go
+++ backend/main.go
@@ -1,41 +1,7 @@
-package cmd
+package main
 
-import (
-	"context"
-	"net"
-	"net/http"
-	"os"
+import "view-list/pkg/cmd"
 
-	"github.com/rs/zerolog/log"
-	"github.com/sethvargo/go-envconfig"
-	"github.com/spf13/cobra"
-)
-
-type Config struct {
-	Hostname string `env:"HTTP_HOST"`
-	Port     string `env:"HTTP_PORT,default=8080"`
-}
-
-var c Config
-
-func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		log.Fatal().Err(err).Msg("failed to execute root command")
-	}
-}
-
-var rootCmd = &cobra.Command{
-	Use: "view-list-api",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		r := routes.Setup()
-		listenAddr := net.JoinHostPort(os.Getenv("HTTP_HOST"), os.Getenv("HTTP_PORT"))
-		log.Fatal().Err(http.ListenAndServe(listenAddr, r)).Msg("failed to start server")
-		return nil
-	},
-}
-
-func init() {
-	if err := envconfig.Process(context.Background(), &c); err != nil {
-		log.Fatal().Err(err).Msg("failed to process environment variables")
-	}
+func main() {
+	cmd.Execute()
 }
